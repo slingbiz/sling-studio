d1c4e82693066668a58b179e4995def99f99771d
Not Guest <not-guest@Administrators-MacBook-Pro.local>
2021-05-17 23:16:35 +0400

    latest

---
+++ modules/pages/PagesSideBar/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import {useSelector} from 'react-redux';
+// import AddNewTask from '../AddNewTask';
+import IntlMessages from '../../../@crema/utility/IntlMessages';
+import Scrollbar from '../../../@crema/core/Scrollbar';
+import AppsSideBarFolderItem from '../../../@crema/core/AppsSideBarFolderItem';
+import LabelItem from './LabelItem';
+import {Fonts} from '../../../shared/constants/AppEnums';
+import {makeStyles} from '@material-ui/core/styles';
+import AppList from '../../../@crema/core/AppList';
+import AppAnimate from '../../../@crema/core/AppAnimate';
+import ListEmptyResult from '../../../@crema/core/AppList/ListEmptyResult';
+import SidebarPlaceholder from '../../../@crema/core/Skeleton/SidebarListSkeleton';
+import folderList from '../../../@crema/services/db/apps/default/folderList';
+import {useRouter} from 'next/router';
+
+const useStyle = makeStyles((theme) => ({
+  appsSidebar: {
+    height: '100%',
+    [theme.breakpoints.up('lg')]: {
+      width: '12rem',
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: '15rem',
+    },
+  },
+  btnRoot: {
+    width: '100%',
+    fontSize: 16,
+    fontWeight: Fonts.MEDIUM,
+    textTransform: 'capitalize',
+    [theme.breakpoints.up('xl')]: {
+      fontSize: 20,
+    },
+  },
+}));
+
+const PagesSideBar = ({basePath}) => {
+  const [isAddTaskOpen, setAddTaskOpen] = React.useState(false);
+
+  const classes = useStyle();
+
+  return (
+    <>
+      <Scrollbar className='scroll-app-sidebar'>
+        <Box p={0} m={0} style={{textAlign: 'center'}}>
+          <Box clone>
+            <List component='nav' aria-label='main task folders'>
+              <AppList
+                pageClasses={classes}
+                data={folderList}
+                ListEmptyComponent={
+                  <ListEmptyResult
+                    loading={true}
+                    placeholder={<SidebarPlaceholder />}
+                  />
+                }
+                renderRow={(item) => (
+                  <AppsSideBarFolderItem
+                    key={item.id}
+                    item={item}
+                    path={`${basePath}/${item.alias}`}
+                  />
+                )}
+              />
+            </List>
+          </Box>
+
+          {/*<Box component='h5' mt={{xs: 4, xl: 5}} fontWeight={Fonts.MEDIUM}>*/}
+          {/*  Labels*/}
+          {/*</Box>*/}
+
+          {/*<List component='nav' aria-label='main mailbox folders'>*/}
+          {/*  <AppList*/}
+          {/*    data={labelList}*/}
+          {/*    ListEmptyComponent={*/}
+          {/*      <ListEmptyResult*/}
+          {/*        loading={true}*/}
+          {/*        placeholder={<SidebarPlaceholder />}*/}
+          {/*      />*/}
+          {/*    }*/}
+          {/*    renderRow={(label) => <LabelItem key={label.id} label={label} />}*/}
+          {/*  />*/}
+          {/*</List>*/}
+        </Box>
+      </Scrollbar>
+
+      {isAddTaskOpen ? (
+        <>
+          {/*// <AddNewTask*/}
+          {/*//   isAddTaskOpen={isAddTaskOpen}*/}
+          {/*//   onCloseAddTask={onCloseAddTask}*/}
+          {/*// />*/}
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default PagesSideBar;
---
+++ modules/pages/index.js
@@ -0,0 +1,93 @@
+import React, {useEffect} from 'react';
+import PagesSideBar from './PagesSideBar/index';
+import TasksList from './TasksList';
+import TaskDetail from './TaskDetail';
+import {useDispatch} from 'react-redux';
+import {capitalize} from '@material-ui/core/utils';
+
+import {
+  onGetToDoFolderList,
+  onGetToDoLabelList,
+  onGetToDoPriorityList,
+  onGetToDoStaffList,
+  onGetToDoStatusList,
+} from '../../redux/actions';
+import PropTypes from 'prop-types';
+import {useIntl} from 'react-intl';
+import AppsContainer from '../../@crema/core/AppsContainer';
+import {useRouter, withRouter} from 'next/router';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+
+const useStyle = makeStyles((theme) => ({
+  appsSidebar: {
+    margin: '0',
+    padding: '0',
+    height: '100%',
+    [theme.breakpoints.up('lg')]: {
+      width: '12rem',
+    },
+    [theme.breakpoints.up('xl')]: {
+      width: '15rem',
+    },
+  },
+}));
+
+const Index = (props) => {
+  const dispatch = useDispatch();
+  const classes = useStyle();
+
+  useEffect(() => {
+    dispatch(onGetToDoLabelList());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(onGetToDoFolderList());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(onGetToDoPriorityList());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(onGetToDoStaffList());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(onGetToDoStatusList());
+  }, [dispatch]);
+
+  const onGetMainComponent = () => {
+    if (+props.router.query.all[1] > 0) {
+      return <TaskDetail />;
+    } else {
+      return <TasksList />;
+    }
+  };
+
+  const getTitle = () => {
+    const titleKey = props.router.query.all.join('.');
+    return messages[titleKey] || capitalize(titleKey);
+  };
+
+  const {messages} = useIntl();
+  const basePath = `/pages/${props.router.query.all[0]}`;
+
+  return (
+    <AppsContainer
+      pagesClasses={classes}
+      title={getTitle()}
+      sidebarContent={<PagesSideBar basePath={basePath} />}>
+      {onGetMainComponent()}
+    </AppsContainer>
+  );
+};
+
+export default withRouter(Index);
+
+Index.defaultProps = {
+  match: null,
+};
+
+Index.prototype = {
+  match: PropTypes.node,
+};
