59a1606b841d6eb7a8ba30a5ca63258cebfc5c65
aws <admin@aws>
2021-08-17 19:33:17 +0000

    changes on aws stage

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   // optimizeFonts: false,
   // useFileSystemPublicRoutes: false,
-  basePath: '/frontend',
-  assetPrefix: 'frontend',
+ // basePath: '/',
+ // assetPrefix: 'frontend',
   distDir: 'build',
   env: {
     FIREBASE_API_KEY: 'AIzaSyAzL_2jiVBhmiIUFGs2z6-cDR-Hgoedh3k',
     FIREBASE_APP_ID: '1:369173776768:web:895ded916749deebd31965',
     FIREBASE_MESSAGING_SENDER_ID: '369173776768',
-    FIREBASE_MEASUREMENT_ID: 'G-976YVMRB4R',
+imageBasePath:'../',
+	  FIREBASE_MEASUREMENT_ID: 'G-976YVMRB4R',
   },
 };
+
