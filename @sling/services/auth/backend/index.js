506fe17271c170b09ebc35bc7090a6293d158f3c
ankur <ankur@sling.biz>
2022-03-17 23:58:18 +0400

    adding auth

--- @sling/services/auth/backend/index.js
+++ @sling/services/auth/backend/index.js
@@ -1,7 +1,11 @@
-import API from '../../ApiConfig.js';
+import ApiAuth from '../../ApiAuthConfig.js';
 
-const sendVerificationEmail = (token) => {
-  return API.post(
+const sendVerificationEmail = async (token) => {
+  const Api = await ApiAuth();
+  if (!Api) {
+    return;
+  }
+  return Api.post(
     `${process.env.NEXT_PUBLIC_SERVICE_URL}/v1/auth/send-verification-email`,
     {},
     {
@@ -12,16 +16,24 @@ const sendVerificationEmail = (token) => {
   );
 };
 
-const registerUser = (name, email, password) => {
-  return API.post(`${process.env.NEXT_PUBLIC_SERVICE_URL}/v1/auth/register`, {
+const registerUser = async (name, email, password) => {
+  const Api = await ApiAuth();
+  if (!Api) {
+    return;
+  }
+  return Api.post(`${process.env.NEXT_PUBLIC_SERVICE_URL}/v1/auth/register`, {
     name: name,
     email: email,
     password: password,
   });
 };
 
-const loginUser = (email, password) => {
-  return API.post(`${process.env.NEXT_PUBLIC_SERVICE_URL}/v1/auth/login`, {
+const loginUser = async (email, password) => {
+  const Api = await ApiAuth();
+  if (!Api) {
+    return;
+  }
+  return Api.post(`${process.env.NEXT_PUBLIC_SERVICE_URL}/v1/auth/login`, {
     email: email,
     password: password,
   });
