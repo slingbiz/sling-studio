3363ec13fcb79eb1c591dd599060a6614aecdc81
ankur <ankur@sling.biz>
2022-03-28 17:14:47 +0400

    add-widget

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const {
 } = require('next/constants');
 
 module.exports = (phase) => {
+  const {
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId,
+  } = process.env;
   if (phase === PHASE_PRODUCTION_BUILD) {
     return {
       eslint: {
@@ -19,11 +29,16 @@ module.exports = (phase) => {
         GET_ROUTES_LIST_API:
           'https://sling.biz/api/v1/pageRoutes/dash/getRoutes',
         SET_CONFIG: 'https://sling.biz/api/v1/dashboard/setConfig',
-        FIREBASE_API_KEY: 'AIzaSyAzL_2jiVBhmiIUFGs2z6-cDR-Hgoedh3k',
-        FIREBASE_APP_ID: '1:369173776768:web:895ded916749deebd31965',
-        FIREBASE_MESSAGING_SENDER_ID: '369173776768',
-        FIREBASE_MEASUREMENT_ID: 'G-976YVMRB4R',
-        NEXT_PUBLIC_GA_ID: 'G-SKGN07R7F1',
+        FIREBASE_JSON: {
+          apiKey,
+          authDomain,
+          databaseURL,
+          projectId,
+          storageBucket,
+          messagingSenderId,
+          appId,
+          measurementId,
+        },
       },
     };
   }
@@ -41,16 +56,16 @@ module.exports = (phase) => {
       GET_ROUTES_LIST_API:
         'http://localhost:10001/v1/pageRoutes/dash/getRoutes',
       SET_CONFIG: 'http://localhost:10001/v1/dashboard/setConfig',
-      // FIREBASE_API_KEY: 'AIzaSyAzL_2jiVBhmiIUFGs2z6-cDR-Hgoedh3k',
-      // FIREBASE_APP_ID: '1:369173776768:web:895ded916749deebd31965',
-      // FIREBASE_MESSAGING_SENDER_ID: '369173776768',
-      // FIREBASE_MEASUREMENT_ID: 'G-976YVMRB4R',
-      // NEXT_PUBLIC_GA_ID: 'G-SKGN07R7F1',
-      FIREBASE_API_KEY: 'AIzaSyD-hMawNOYwuEb7SX7gOimG0hDaJcbH_eI',
-      FIREBASE_APP_ID: '1:386706820447:web:1e71fc0c37b7028f5cfaea',
-      FIREBASE_MESSAGING_SENDER_ID: '386706820447',
-      FIREBASE_MEASUREMENT_ID: 'G-HETEPRNM8J',
-      NEXT_PUBLIC_GA_ID: 'G-SKGN07R7F1',
+      FIREBASE_JSON: {
+        apiKey,
+        authDomain,
+        databaseURL,
+        projectId,
+        storageBucket,
+        messagingSenderId,
+        appId,
+        measurementId,
+      },
     },
   };
 };
