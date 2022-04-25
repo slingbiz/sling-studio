f09edef504b85893a510ab6076224cdc765ebd0e
ankur <ankur@sling.biz>
2022-04-25 18:59:18 +0400

    Add default user email and password

--- modules/auth/Signin/SigninFirebase.js
+++ modules/auth/Signin/SigninFirebase.js
@@ -131,8 +131,8 @@ const SigninFirebase = (props) => {
         <Formik
           validateOnChange={true}
           initialValues={{
-            email: 'hello@slingfe.com',
-            password: 'slingfe',
+            email: 'demo@sling.biz',
+            password: 'demo@sling.biz',
           }}
           validationSchema={validationSchema}
           onSubmit={(data, {setSubmitting}) => {
